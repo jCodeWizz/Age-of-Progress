5d26cfdb6d24b39ca14263bc5375e6a2ab5a2834
CodeWizz <tom.heijkamp@outlook.com>
2023-12-03 09:50:31 +0100

    Updated test mod
    Removed a random Logger.log

--- TestMod/src/mod/main/Main.java
+++ TestMod/src/mod/main/Main.java
@@ -2,7 +2,6 @@ package mod.main;
 
 import dev.codewizz.modding.JavaMod;
 import dev.codewizz.modding.Registers;
-import dev.codewizz.utils.Logger;
 
 public class Main extends JavaMod {
 	
@@ -13,7 +12,6 @@ public class Main extends JavaMod {
 
 	@Override
 	public void onStart() {
-		Logger.log("registering");
 		Registers.registerEvent(info, new OpenWorldEvent());
 	}
 	
