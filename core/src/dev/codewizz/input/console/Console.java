a982d0cc20f905468de3d11992eb9f38db4dc6e2
CodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2024-07-04 17:46:09 +0200

    fixex console not closing x2

--- core/src/dev/codewizz/input/console/Console.java
+++ core/src/dev/codewizz/input/console/Console.java
@@ -4,14 +4,17 @@ import dev.codewizz.input.console.commands.*;
 import dev.codewizz.main.Main;
 import dev.codewizz.modding.Registers;
 import dev.codewizz.utils.Logger;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 public class Console {
 
-    private Scanner scanner;
+    private BufferedReader reader;
     private Thread consoleThread;
+    private volatile boolean running;
 
     public void register() {
         Registers.registerCommand("time", new TimeCommand());
@@ -24,44 +27,50 @@ public class Console {
     }
 
     public void start() {
+        running = true;
         consoleThread = new Thread("console") {
             public void run() {
-                scanner = new Scanner(System.in);
-
-
+                reader = new BufferedReader(new InputStreamReader(System.in));
                 Logger.log(Main.PLAYING + " " + Main.RUNNING + " " + this.isInterrupted());
 
-
-                while((Main.PLAYING || Main.RUNNING) && !this.isInterrupted()) {
+                while((Main.PLAYING || Main.RUNNING) && running && !Thread.currentThread().isInterrupted()) {
                     try {
-                        String command = scanner.nextLine();
-                        Logger.log("Executing command: '" + command + "'!");
-                        boolean success = false;
-                        if(command.contains(" ")) {
-                            String name = command.split(" ")[0];
-                            String[] values = command.split(" ");
-                            String[] args = Arrays.copyOfRange(values, 1, values.length);
-                            if(Registers.commands.containsKey(name)) {
-                                success = Registers.commands.get(name).execute(name, Main.inst.world, args);
+                        if (reader.ready()) {
+                            String command = reader.readLine();
+                            Logger.log("Executing command: '" + command + "'!");
+                            boolean success = false;
+                            if(command.contains(" ")) {
+                                String name = command.split(" ")[0];
+                                String[] values = command.split(" ");
+                                String[] args = Arrays.copyOfRange(values, 1, values.length);
+                                if(Registers.commands.containsKey(name)) {
+                                    success = Registers.commands.get(name).execute(name, Main.inst.world, args);
+                                } else {
+                                    Logger.error("couldn't find command '" + name + "'!");
+                                }
                             } else {
-                                Logger.error("couldn't find command '" + name + "'!");
+                                if(Registers.commands.containsKey(command)) {
+                                    success = Registers.commands.get(command).execute(command, Main.inst.world, new String[] {});
+                                } else {
+                                    Logger.error("couldn't find command '" + command + "'!");
+                                }
+                            }
+
+                            if(success) {
+                                Logger.log("Command successfully executed!");
+                            } else {
+                                Logger.error("Command could not be executed!");
                             }
                         } else {
-                            if(Registers.commands.containsKey(command)) {
-                                success = Registers.commands.get(command).execute(command, Main.inst.world, new String[] {});
-                            } else {
-                                Logger.error("couldn't find command '" + command + "'!");
-                            }
+                            Thread.sleep(100); // Sleep for a short period to avoid busy-waiting
                         }
-
-                        if(success) {
-                            Logger.log("Command successfully executed!");
-                        } else {
-                            Logger.error("Command could not be executed!");
+                    } catch (IOException e) {
+                        if (running) {
+                            Logger.error("IOException while reading command: ");
+                            e.printStackTrace();
                         }
-
-                    } catch (NoSuchElementException ignored) {
-                        Logger.log("V: " + Main.PLAYING + " " + Main.RUNNING);
+                    } catch (InterruptedException e) {
+                        break;
                     } catch (Exception e) {
                         Logger.error("Exception while executing command: ");
                         e.printStackTrace();
@@ -74,5 +83,9 @@ public class Console {
 
     public void stop() {
         consoleThread.interrupt();
+        try {
+            reader.close();
+        } catch (IOException ignored) {
+        }
     }
 }
