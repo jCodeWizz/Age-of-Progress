4ff35d6d08e72a0ef8419f42b1ca6e9a7c7e0d7f
CodeWizz <tom.heijkamp@outlook.com>
2024-07-04 14:33:25 +0200

    added new command

---
+++ core/src/dev/codewizz/input/console/commands/RemoveObjectCommand.java
@@ -0,0 +1,29 @@
+package dev.codewizz.input.console.commands;
+
+import dev.codewizz.input.console.CommandExecutor;
+import dev.codewizz.utils.Logger;
+import dev.codewizz.world.GameObject;
+import dev.codewizz.world.World;
+
+public class RemoveObjectCommand implements CommandExecutor {
+
+    @Override
+    public boolean execute(String command, World world, String[] args) {
+        if(args.length > 0) {
+            String id = args[0].contains(":") ? args[0] : "aop:" + args[0];
+
+            int counter = 0;
+            for(GameObject object : world.getGameObjects()) {
+                if(object.getId().equalsIgnoreCase(id)) {
+                    counter++;
+                    object.destroy();
+                }
+            }
+
+            Logger.log("Removed " + counter + " objects!");
+            return true;
+        }
+
+        return false;
+    }
+}
