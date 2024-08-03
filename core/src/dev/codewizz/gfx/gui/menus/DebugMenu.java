418a97aab667081add21902ac7a4c3da09190d6f
CodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2024-08-03 14:14:27 +0200

    added new features to debug menu V2

--- core/src/dev/codewizz/gfx/gui/menus/DebugMenu.java
+++ core/src/dev/codewizz/gfx/gui/menus/DebugMenu.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Array;
-import dev.codewizz.gfx.Particle;
 import dev.codewizz.gfx.gui.elements.UILabel;
 import dev.codewizz.gfx.gui.layers.GameLayer;
 import dev.codewizz.input.MouseInput;
