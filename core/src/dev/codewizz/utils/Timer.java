83c1358eff04f2b9e57dd46568a665350e77cf54
CodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2024-08-13 11:09:19 +0200

    added Timer.java util class

---
+++ core/src/dev/codewizz/utils/Timer.java
@@ -0,0 +1,41 @@
+package dev.codewizz.utils;
+
+public class Timer {
+
+    private float time;
+    private float maxTime;
+
+    private boolean repeat;
+
+    public Timer(float maxTime) {
+        this.time = maxTime;
+        this.maxTime = maxTime;
+
+        repeat = false;
+    }
+
+    public void update(float d) {
+        if (time > 0) {
+            time -= d;
+        } else {
+            finish();
+            if (repeat) {
+                time = maxTime;
+            }
+        }
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public void setMaxTime(float maxTime) {
+        this.maxTime = maxTime;
+    }
+
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+
+    public void finish() {}
+}
