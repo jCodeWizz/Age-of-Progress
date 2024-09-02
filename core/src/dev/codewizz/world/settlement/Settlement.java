241f084de588b29d0957b4675aaac542a199946b
CodeWizz <tom.heijkamp@outlook.com>
2024-09-02 17:45:13 +0200

    removed cheat items, should be stand alone mod.

--- core/src/dev/codewizz/world/settlement/Settlement.java
+++ core/src/dev/codewizz/world/settlement/Settlement.java
@@ -12,168 +12,160 @@ import dev.codewizz.world.GameObject;
 import dev.codewizz.world.building.Building;
 import dev.codewizz.world.items.Inventory;
 import dev.codewizz.world.items.Item;
-import dev.codewizz.world.items.ItemType;
 import dev.codewizz.world.objects.hermits.Hermit;
 import dev.codewizz.world.objects.tasks.GrowCropTask;
 import dev.codewizz.world.objects.tasks.HaulTask;
 import dev.codewizz.world.objects.tasks.Task;
-
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Settlement {
 
-	private float x, y;
-	private float timer = 0, max_timer = 5f;
-	private Cell cell;
-	
-	public List<Hermit> members = new CopyOnWriteArrayList<>();
-	public Queue<Task> taskTree = new Queue<>();
-	public List<GameObject> objects = new CopyOnWriteArrayList<>();
-	public List<Crop> crops = new CopyOnWriteArrayList<>();
-	public List<FarmArea> areas = new CopyOnWriteArrayList<>();
-	public List<Building> buildings = new CopyOnWriteArrayList<>();
-	
-	public Inventory inventory;
+    private float x, y;
+    private float timer = 0, max_timer = 5f; //TODO: make use of Timer object
+    private Cell cell;
 
-	private Settlement() {
-		this.inventory = new Inventory(-1);
+    public List<Hermit> members = new CopyOnWriteArrayList<>();
+    public Queue<Task> taskTree = new Queue<>();
+    public List<GameObject> objects = new CopyOnWriteArrayList<>();
+    public List<Crop> crops = new CopyOnWriteArrayList<>();
+    public List<FarmArea> areas = new CopyOnWriteArrayList<>();
+    public List<Building> buildings = new CopyOnWriteArrayList<>();
+
+    public Inventory inventory;
+
+    private Settlement() {
+        this.inventory = new Inventory(-1);
+    }
+
+    public Settlement(Cell cell) {
+        this();
+        this.x = cell.x + 32;
+        this.y = cell.y + 32;
+        this.cell = cell;
+    }
+
+    public Settlement(float x, float y) {
+        this();
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(float dt) {
+
+        if (timer < max_timer) { timer += dt; } else {
+            timer = 0f;
+            checkHaulTask();
+        }
+
+        if (Main.inst.world.nature.day) {
+            for (Crop crop : crops) {
+                if (crop.isReady()) {
+                    if (!crop.tasked) {
+                        crop.task();
+                        addTask(new GrowCropTask(crop), true);
+                    }
+                } else {
+                    crop.counter += dt;
+                }
+            }
+        }
+    }
+
+    public Hermit addHermit(float x, float y) {
 
 
-		inventory.addItem(new Item(ItemType.WOOD, 100));
-		inventory.addItem(new Item(ItemType.PLANKS, 100));
-	}
+        Hermit hermit = new Hermit(x, y);
 
-	public Settlement(Cell cell) {
-		this();
-		this.x = cell.x + 32;
-		this.y = cell.y + 32;
-		this.cell = cell;
-	}
+        hermit.setSettlement(this);
+        members.add(hermit);
+        Main.inst.world.addObject(hermit, Reason.FORCED);
 
-	public Settlement(float x, float y) {
-		this();
-		this.x = x;
-		this.y = y;
-	}
+        Event.dispatch(new HermitJoinEvent(hermit, this));
 
-	public void update(float dt) {
+        return hermit;
+    }
 
-		if (timer < max_timer)
-			timer += dt;
-		else {
-			timer = 0f;
-			checkHaulTask();
-		}
-		
-		if(Main.inst.world.nature.day) {
-			for(Crop crop : crops) {
-				if(crop.isReady()) {
-					if(!crop.tasked) {
-						crop.task();
-						addTask(new GrowCropTask(crop), true);
-					}
-				} else {
-					crop.counter += dt;
-				}
-			}
-		}
-	}
+    private void checkHaulTask() {
+        List<Item> items = new CopyOnWriteArrayList<>();
 
-	public Hermit addHermit(float x, float y) {
+        for (Renderable r : Main.inst.world.getObjects()) {
+            if (r instanceof Item) {
+                Item i = (Item) r;
+                if (!i.isTasked()) {
+                    items.add(i);
+                }
+            }
+        }
 
-		
-		Hermit hermit = new Hermit(x, y);
+        List<Item> t = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            t.add(items.get(i));
 
-		hermit.setSettlement(this);
-		members.add(hermit);
-		Main.inst.world.addObject(hermit, Reason.FORCED);
+            if (t.size() > 5) {
+                HaulTask task = new HaulTask(t);
+                taskTree.addLast(task);
+                t = new CopyOnWriteArrayList<>();
+            }
+        }
 
-		Event.dispatch(new HermitJoinEvent(hermit, this));
+        if (!t.isEmpty()) {
+            HaulTask task = new HaulTask(t);
+            taskTree.addLast(task);
+        }
+    }
 
-		return hermit;
-	}
+    public Hermit addHermit(Hermit hermit) {
 
-	private void checkHaulTask() {
-		List<Item> items = new CopyOnWriteArrayList<>();
+        hermit.setSettlement(this);
+        members.add(hermit);
 
-		for (Renderable r : Main.inst.world.getObjects()) {
-			if (r instanceof Item) {
-				Item i = (Item) r;
-				if (!i.isTasked()) {
-					items.add(i);
-				}
-			}
-		}
+        return hermit;
+    }
 
-		List<Item> t = new CopyOnWriteArrayList<>();
-		for (int i = 0; i < items.size(); i++) {
-			t.add(items.get(i));
+    public Queue<Task> getTasks() {
+        return taskTree;
+    }
 
-			if (t.size() > 5) {
-				HaulTask task = new HaulTask(t);
-				taskTree.addLast(task);
-				t = new CopyOnWriteArrayList<>();
-			}
-		}
+    public void addTask(Task task, boolean prio) {
 
-		if (!t.isEmpty()) {
-			HaulTask task = new HaulTask(t);
-			taskTree.addLast(task);
-		}
-	}
+        if (prio) {
+            taskTree.addFirst(task);
+        } else {
+            taskTree.addLast(task);
+        }
+    }
 
-	public Hermit addHermit(Hermit hermit) {
+    public Cell getCell() {
+        if (cell == null) { this.cell = Main.inst.world.getCell(x, y); }
+        return cell;
+    }
 
-		hermit.setSettlement(this);
-		members.add(hermit);
+    public void removeTask(Task task) {
+        taskTree.removeValue(task, false);
+    }
 
-		return hermit;
-	}
+    public Vector2 getLocation() {
+        return new Vector2(x, y);
+    }
 
-	public Queue<Task> getTasks() {
-		return taskTree;
-	}
+    public float getX() {
+        return x;
+    }
 
-	public void addTask(Task task, boolean prio) {
+    public void setX(float x) {
+        this.x = x;
+    }
 
-		if (prio) {
-			taskTree.addFirst(task);
-		} else {
-			taskTree.addLast(task);
-		}
-	}
+    public float getY() {
+        return y;
+    }
 
-	public Cell getCell() {
-		if(cell == null) this.cell = Main.inst.world.getCell(x, y);
-		return cell;
-	}
+    public void setY(float y) {
+        this.y = y;
+    }
 
-	public void removeTask(Task task) {
-		taskTree.removeValue(task, false);
-	}
-
-	public Vector2 getLocation() {
-		return new Vector2(x, y);
-	}
-
-	public float getX() {
-		return x;
-	}
-
-	public void setX(float x) {
-		this.x = x;
-	}
-
-	public float getY() {
-		return y;
-	}
-
-	public void setY(float y) {
-		this.y = y;
-	}
-	
-	public Inventory getInventory() {
-		return this.inventory;
-	}
+    public Inventory getInventory() {
+        return this.inventory;
+    }
 }
