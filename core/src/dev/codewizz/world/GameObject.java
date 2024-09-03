f9d3bf66acc43f25bba5d4fb8280c011b4826cee
CodeWizz <tom.heijkamp@outlook.com>
2024-09-03 17:05:05 +0200

    changed select menu of hermit a little

--- core/src/dev/codewizz/world/GameObject.java
+++ core/src/dev/codewizz/world/GameObject.java
@@ -13,231 +13,224 @@ import dev.codewizz.utils.saving.GameObjectData;
 import dev.codewizz.utils.saving.GameObjectDataLoader;
 import dev.codewizz.utils.serialization.ByteUtils;
 import dev.codewizz.utils.serialization.SerializableObject;
-
 import java.awt.*;
 import java.util.UUID;
 
 public abstract class GameObject extends Renderable implements SerializableObject {
 
-	protected String id = "unid-object";
-	protected UUID uuid;
+    protected String id = "unid-object";
+    protected UUID uuid;
 
-	protected float x, y, sortHeight;
-	protected int w, h;
-	protected boolean flip = false;
-	protected Cell cell;
-	
-	protected boolean selected = false;
-	protected String name = "Object";
-	
-	public GameObject() {
-		
-	}
-	
-	public GameObject(float x, float y) {
-		uuid = UUID.randomUUID();
+    protected float x, y, sortHeight;
+    protected int w, h;
+    protected boolean flip = false;
+    protected Cell cell;
 
-		this.x = x;
-		this.y = y;
-	}
-	
-	public abstract void update(float d);
-	public abstract void render(SpriteBatch b);
-	
+    protected boolean selected = false;
+    protected String name = "Object";
 
-	@Override
-	public GameObjectData save(GameObjectData object) {
+    public GameObject() {
 
-		object.addByte(ByteUtils.toByte((byte)0, flip, 0));
+    }
 
-		object.addFloat(x);
-		object.addFloat(y);
-		object.addInteger(w);
-		object.addInteger(h);
+    public GameObject(float x, float y) {
+        uuid = UUID.randomUUID();
 
-		object.addString(name);
-		object.end();
-		return object;
-	}
-	
-	@Override
-	public boolean load(GameObjectDataLoader loader, GameObjectData object, boolean success) {
-		byte[] data = object.take();
+        this.x = x;
+        this.y = y;
+    }
 
-		flip = ByteUtils.toBoolean(data[0], 0);
+    public abstract void update(float d);
 
-		x = ByteUtils.toFloat(data, 1);
-		y = ByteUtils.toFloat(data, 5);
-		w = ByteUtils.toInteger(data, 9);
-		h = ByteUtils.toInteger(data, 13);
-		
-		name = ByteUtils.toString(data, 17);
+    public abstract void render(SpriteBatch b);
 
-		return success;
-	}
 
-	public void renderDebug() {
-		Polygon g = this.getHitBox();
-		
-		for(int i = 0; i < g.npoints; i++) {
-			Renderer.drawDebugLine(new Vector2(g.xpoints[i % g.npoints], g.ypoints[i % g.npoints]), new Vector2(g.xpoints[(i+1) % g.npoints], g.ypoints[(i+1) % g.npoints]));
-		}
-	}
-	
-	public void onDestroy() {}
-	
-	public void destroy() {
-		if(cell != null) cell.setObject(null);
+    @Override
+    public GameObjectData save(GameObjectData object) {
 
-		if(this.isSelected()) this.deselect();
-		
-		onDestroy();
-		Main.inst.world.removeObject(this);
-		cell = null;
-	}
-	
-	public void select() {
-		selected = true;
-		((GameLayer) Main.inst.renderer.uiLayer).openMenu(((GameLayer) Main.inst.renderer.uiLayer).selectMenu);
-		((GameLayer) Main.inst.renderer.uiLayer).selectMenu.setSelected(this);
-		((GameLayer) Main.inst.renderer.uiLayer).selectMenu.updateData();
-	}
-	
-	public void deselect() {
-		((GameLayer) Main.inst.renderer.uiLayer).closeMenus();
-		((GameLayer) Main.inst.renderer.uiLayer).selectMenu.setSelected(null);
-		selected = false;
-	}
+        object.addByte(ByteUtils.toByte((byte) 0, flip, 0));
 
-	private UILabel nameLabel;
+        object.addFloat(x);
+        object.addFloat(y);
+        object.addInteger(w);
+        object.addInteger(h);
 
-	public void setupSelectMenu(Table top, Table bottom) {
-		nameLabel = UILabel.create(name);
+        object.addString(name);
+        object.end();
+        return object;
+    }
 
-		top.add(nameLabel).expand().fill().center().left().padLeft(6 * Layer.scale);
-	}
+    @Override
+    public boolean load(GameObjectDataLoader loader, GameObjectData object, boolean success) {
+        byte[] data = object.take();
 
-	public void updateSelectMenu() {
-		nameLabel.setText(name);
-	}
+        flip = ByteUtils.toBoolean(data[0], 0);
 
-	public Rectangle getBounds() {
-		return new Rectangle((int)x, (int)y, w, h);
-	}
-	
-	public Polygon getHitBox() {
-		return new Polygon( new int[] {(int)x + w/2, (int)x, (int)x + w/2, (int)x + w}, new int[] {(int)y, (int)y + h/2, (int)y + h, (int)y + h/2}, 4) ;
-	}
+        x = ByteUtils.toFloat(data, 1);
+        y = ByteUtils.toFloat(data, 5);
+        w = ByteUtils.toInteger(data, 9);
+        h = ByteUtils.toInteger(data, 13);
 
-	public float getX() {
-		return x;
-	}
+        name = ByteUtils.toString(data, 17);
 
-	public void setX(float x) {
-		this.x = x;
-	}
+        return success;
+    }
 
-	public float getY() {
-		return y;
-	}
+    public void renderDebug() {
+        Polygon g = this.getHitBox();
 
-	public void setY(float y) {
-		this.y = y;
-	}
+        for (int i = 0; i < g.npoints; i++) {
+            Renderer.drawDebugLine(new Vector2(g.xpoints[i % g.npoints], g.ypoints[i % g.npoints]),
+                                   new Vector2(g.xpoints[(i + 1) % g.npoints],
+                                               g.ypoints[(i + 1) % g.npoints]));
+        }
+    }
 
-	public int getW() {
-		return w;
-	}
+    public void onDestroy() { }
 
-	public void setW(int w) {
-		this.w = w;
-	}
+    public void destroy() {
+        if (cell != null) { cell.setObject(null); }
 
-	public int getH() {
-		return h;
-	}
+        if (this.isSelected()) { this.deselect(); }
 
-	public void setH(int h) {
-		this.h = h;
-	}
-	
-	public String getId() {
-		return id;
-	}
+        onDestroy();
+        Main.inst.world.removeObject(this);
+        cell = null;
+    }
 
-	public boolean isSelected() {
-		return selected;
-	}
+    public void select() {
+        selected = true;
+        ((GameLayer) Main.inst.renderer.uiLayer).openMenu(
+                ((GameLayer) Main.inst.renderer.uiLayer).selectMenu);
+        ((GameLayer) Main.inst.renderer.uiLayer).selectMenu.setSelected(this);
+        ((GameLayer) Main.inst.renderer.uiLayer).selectMenu.updateData();
+    }
 
-	public void setSelected(boolean selected) {
-		this.selected = selected;
-	}
+    public void deselect() {
+        ((GameLayer) Main.inst.renderer.uiLayer).closeMenus();
+        ((GameLayer) Main.inst.renderer.uiLayer).selectMenu.setSelected(null);
+        selected = false;
+    }
 
-	public String getName() {
-		return name;
-	}
+    protected UILabel nameLabel;
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	@Override
-	public float getSortingHeight() {
-		return sortHeight;
-	}
+    public void setupSelectMenu(Table top, Table bottom) {
+        nameLabel = UILabel.create(name);
 
-	public boolean isFlip() {
-		return flip;
-	}
+        top.add(nameLabel).expand().fill().center().left().padLeft(6 * Layer.scale);
+        top.row();
+    }
 
-	public void setFlip(boolean flip) {
-		this.flip = flip;
-	}
-	
-	public GameObject flip() {
-		this.flip = !this.flip;
-		
-		return this;
-	}
-	
-	public Vector2 getFootPoint() {
-		return new Vector2(this.x + this.w/2f, this.y);
-	}
-	
-	public void setCell(Cell cell) {
-		this.cell = cell;
-	}
-	
-	public Cell getCell() {
-		return this.cell;
-	}
+    public void updateSelectMenu() {
+        nameLabel.setText(name);
+    }
 
-	public UUID getUUID() {
-		return uuid;
-	}
+    public Rectangle getBounds() {
+        return new Rectangle((int) x, (int) y, w, h);
+    }
 
-	public Vector2 getCenter() {
-		return new Vector2(x + (w/2f), y);
-	}
+    public Polygon getHitBox() {
+        return new Polygon(new int[]{(int) x + w / 2, (int) x, (int) x + w / 2, (int) x + w},
+                           new int[]{(int) y, (int) y + h / 2, (int) y + h, (int) y + h / 2}, 4);
+    }
 
-	@Override
-	public String toString() {
-		return "GameObject{" +
-				"id='" + id + '\'' +
-				", uuid=" + uuid +
-				", x=" + x +
-				", y=" + y +
-				", sortHeight=" + sortHeight +
-				", w=" + w +
-				", h=" + h +
-				", flip=" + flip +
-				", cell=" + cell +
-				", selected=" + selected +
-				", name='" + name + '\'' +
-				'}';
-	}
+    public float getX() {
+        return x;
+    }
 
-	public void setUUID(UUID uuid) {
-		this.uuid = uuid;
-	}
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public float getSortingHeight() {
+        return sortHeight;
+    }
+
+    public boolean isFlip() {
+        return flip;
+    }
+
+    public void setFlip(boolean flip) {
+        this.flip = flip;
+    }
+
+    public GameObject flip() {
+        this.flip = !this.flip;
+
+        return this;
+    }
+
+    public Vector2 getFootPoint() {
+        return new Vector2(this.x + this.w / 2f, this.y);
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    public Cell getCell() {
+        return this.cell;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public Vector2 getCenter() {
+        return new Vector2(x + (w / 2f), y);
+    }
+
+    @Override
+    public String toString() {
+        return "GameObject{" + "id='" + id + '\'' + ", uuid=" + uuid + ", x=" + x + ", y=" + y + ", sortHeight=" + sortHeight + ", w=" + w + ", h=" + h + ", flip=" + flip + ", cell=" + cell + ", selected=" + selected + ", name='" + name + '\'' + '}';
+    }
+
+    public void setUUID(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
--- core/src/dev/codewizz/world/objects/hermits/Hermit.java
+++ core/src/dev/codewizz/world/objects/hermits/Hermit.java
@@ -1,11 +1,12 @@
 package dev.codewizz.world.objects.hermits;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import dev.codewizz.gfx.Animation;
-import dev.codewizz.gfx.Particle;
 import dev.codewizz.main.Main;
 import dev.codewizz.utils.Direction;
 import dev.codewizz.utils.Utils;
@@ -19,224 +20,240 @@ import dev.codewizz.world.objects.buildings.Building;
 import dev.codewizz.world.objects.tasks.ClearInventoryTask;
 import dev.codewizz.world.objects.tasks.Task;
 import dev.codewizz.world.settlement.Settlement;
-
 import java.awt.*;
 
 public class Hermit extends TaskableObject implements SerializableObject {
 
-	public static final float walkAnimSpeed = 0.1f;
-	
-	private Direction dir = Direction.South;
+    public static final float walkAnimSpeed = 0.1f;
 
-	private Animation taskAnimation;
-	private Animation currentAnimation;
-	private Sprite currentDirection;
-	
-	private Settlement s;
-	private Building home;
-	private Job job;
-	private Inventory inventory;
-	
-	//private float intelligence = 1f;
-	//private float strength = 1f;
-	//private float speed = 1f;
-	//private float willpower = 1f;
-	private float sleep = 1f;
-	private float healthy = Utils.RANDOM.nextFloat();
-	private float body = Utils.RANDOM.nextFloat();
-	//private float social = 1f;
-	//private float hunger = 1f;
-	//private float thirst = 1f;
-	
-	private float sleepNeed = 10f;
-	
-	private int age = 101;
-	
-	public Hermit() {
-		super();
-		
-		this.id = "aop:hermit";
-		this.inventory = new Inventory(5);
-		this.sortHeight = 5;
+    private Direction dir = Direction.South;
 
-		this.setJob(new Worker());
-	}
-	
-	public Hermit(float x, float y) {
-		super(x, y);
-		this.id = "aop:hermit";
-		this.name = Utils.getRandomName();
-		this.inventory = new Inventory(5);
-		
-		this.w = 24;
-		this.h = 36;
-		this.sortHeight = 5;
-		this.health = 10f;
-		this.maxHealth = 10f;
-		
-		this.speed = 20f;
+    private Animation taskAnimation;
+    private Animation currentAnimation;
+    private Sprite currentDirection;
 
-		setMaxHealth(0f);
-		setSleepNeed();
+    private Settlement s;
+    private Building home;
+    private Job job;
+    private Inventory inventory;
 
-		this.setJob(new Worker());
-	}
-	
-	@Override
-	public void update(float d) {
-		super.update(d);
-		
-		if(this.currentTask == null && s.getTasks().notEmpty()) {
-			
-			for(Task task : s.getTasks()) {
-				
-				if(task.canTake(this)) {
-					this.addTask(task, false);
-					s.getTasks().removeValue(task, false);
-					break;
-				}
-			}
-		}
-		
-		if(this.currentTask == null && !this.getInventory().getItems().isEmpty()) {
-			this.addTask(new ClearInventoryTask(), true);
-		}
-		
-		if(currentAnimation != null) {
-			currentAnimation.tick(d);
-		}
-		
-		if(taskAnimation != null) {
-			taskAnimation.tick(d);
-		}
-		
-		if(this.getAgent().moving) {
-			dir = Utils.getDirFromVector(vel);	
-			currentAnimation = job.animations.get(dir);
-		} else {
-			currentDirection = job.directions.get(dir);
-		}
-		
-		job.update(d);
-	}
-	
-	
+    //private float intelligence = 1f;
+    //private float strength = 1f;
+    //private float speed = 1f;
+    //private float willpower = 1f;
+    private float sleep = 1f;
+    private float healthy = Utils.RANDOM.nextFloat();
+    private float body = Utils.RANDOM.nextFloat();
+    //private float social = 1f;
+    //private float hunger = 1f;
+    //private float thirst = 1f;
 
-	@Override
-	public void render(SpriteBatch b) {
-		
-		if(this.taskAnimation != null) {
-			b.draw(this.taskAnimation.getFrame(), x + this.taskAnimation.getX(), y + this.taskAnimation.getY(), this.taskAnimation.getFrame().getWidth()*0.6f, this.taskAnimation.getFrame().getHeight()*0.6f);
-		} else {
-			if(this.getAgent().moving) {
-				if(currentAnimation != null) {
-					b.draw(currentAnimation.getFrame(), x, y, w, h);
-				}
-			} else {
-				if(currentDirection != null) {
-					b.draw(currentDirection, x, y, w, h);
-				}
-			}
-		}
-		
-		job.render(b);
-	}
+    private float sleepNeed = 10f;
 
-	@Override
-	public Polygon getHitBox() {
-		return new Polygon( new int[] {(int)x+7, (int)x+7, (int)x + 17, (int)x + 17}, 
-							new int[] {(int)y + 3, (int)y + 24, (int)y + 24, (int)y + 3}, 4) ;
-	}
+    private int age = 101;
 
-	public Settlement getSettlement() {
-		return s;
-	}
+    public Hermit() {
+        super();
 
-	public void setSettlement(Settlement s) {
-		this.s = s;
-	}
+        this.id = "aop:hermit";
+        this.inventory = new Inventory(5);
+        this.sortHeight = 5;
 
-	@Override
-	public GameObjectData save(GameObjectData object) {
-		return super.save(object);
-	}
+        this.setJob(new Worker());
+    }
 
-	@Override
-	public boolean load(GameObjectDataLoader loader, GameObjectData object, boolean success) {
-		Main.inst.world.settlement.addHermit(this);
-		return super.load(loader, object, success);
-	}
-	
-	@Override
-	public void setMaxHealth(float maxHealth) {
-		float a = this.getMaxHealth();
-		
-		this.maxHealth = 50f + ((healthy+0.5f) * 20f) + ((body + 0.1f) * 50f);
-		
-		float d = this.getMaxHealth() / a;
-		this.setHealth(this.getHealth() * d);
-	}
-	
-	public void setSleepNeed() {
-		this.sleepNeed = sleep * 100f;
-	}
+    public Hermit(float x, float y) {
+        super(x, y);
+        this.id = "aop:hermit";
+        this.name = Utils.getRandomName();
+        this.inventory = new Inventory(5);
 
-	public float getSleepNeed() {
-		return sleepNeed;
-	}
+        this.w = 24;
+        this.h = 36;
+        this.sortHeight = 5;
+        this.health = 10f;
+        this.maxHealth = 10f;
 
-	public void setSleepNeed(float sleepNeed) {
-		this.sleepNeed = sleepNeed;
-	}
+        this.speed = 20f;
 
-	public Building getHome() {
-		
-		if(this.home == null) {
-			
-			for(GameObject object : s.objects) {
-				if(object instanceof Building) {
-					Building b = (Building) object;
-					if(!b.isFull()) {
-						b.owned.add(this);
-						this.home = b;
-						return b;
-					}
-				}
-			}
-			return null;
-		} else {
-			return home;
-		}
-	}
+        setMaxHealth(0f);
+        setSleepNeed();
 
-	public void setHome(Building home) {
-		this.home = home;
-	}
-	
-	
-	
-	public void setFacing(Direction dir) {
-		this.dir = dir;
-	}
+        this.setJob(new Worker());
+    }
 
-	public Animation getTaskAnimation() {
-		return taskAnimation;
-	}
+    @Override
+    public void update(float d) {
+        super.update(d);
 
-	public void setTaskAnimation(Animation taskAnimation) {
-		this.taskAnimation = taskAnimation;
-	}
-	
-	public Job getJob() {
-		return this.job;
-	}
-	
-	public void setJob(Job job) {
-		this.job = job;
-		this.job.setHermit(this);
-	}
-	
-	public Inventory getInventory() {
-		return this.inventory;
-	}
+        if (this.currentTask == null && s.getTasks().notEmpty()) {
+
+            for (Task task : s.getTasks()) {
+
+                if (task.canTake(this)) {
+                    this.addTask(task, false);
+                    s.getTasks().removeValue(task, false);
+                    break;
+                }
+            }
+        }
+
+        if (this.currentTask == null && !this.getInventory().getItems().isEmpty()) {
+            this.addTask(new ClearInventoryTask(), true);
+        }
+
+        if (currentAnimation != null) {
+            currentAnimation.tick(d);
+        }
+
+        if (taskAnimation != null) {
+            taskAnimation.tick(d);
+        }
+
+        if (this.getAgent().moving) {
+            dir = Utils.getDirFromVector(vel);
+            currentAnimation = job.animations.get(dir);
+        } else {
+            currentDirection = job.directions.get(dir);
+        }
+
+        job.update(d);
+    }
+
+
+    @Override
+    public void render(SpriteBatch b) {
+
+        if (this.taskAnimation != null) {
+            b.draw(this.taskAnimation.getFrame(), x + this.taskAnimation.getX(),
+                   y + this.taskAnimation.getY(), this.taskAnimation.getFrame().getWidth() * 0.6f,
+                   this.taskAnimation.getFrame().getHeight() * 0.6f);
+        } else {
+            if (this.getAgent().moving) {
+                if (currentAnimation != null) {
+                    b.draw(currentAnimation.getFrame(), x, y, w, h);
+                }
+            } else {
+                if (currentDirection != null) {
+                    b.draw(currentDirection, x, y, w, h);
+                }
+            }
+        }
+
+        job.render(b);
+    }
+
+    @Override
+    public Polygon getHitBox() {
+        return new Polygon(new int[]{(int) x + 7, (int) x + 7, (int) x + 17, (int) x + 17},
+                           new int[]{(int) y + 3, (int) y + 24, (int) y + 24, (int) y + 3}, 4);
+    }
+
+    public Settlement getSettlement() {
+        return s;
+    }
+
+    public void setSettlement(Settlement s) {
+        this.s = s;
+    }
+
+    @Override
+    public GameObjectData save(GameObjectData object) {
+        return super.save(object);
+    }
+
+    @Override
+    public boolean load(GameObjectDataLoader loader, GameObjectData object, boolean success) {
+        Main.inst.world.settlement.addHermit(this);
+        return super.load(loader, object, success);
+    }
+
+    @Override
+    public void setMaxHealth(float maxHealth) {
+        float a = this.getMaxHealth();
+
+        this.maxHealth = 50f + ((healthy + 0.5f) * 20f) + ((body + 0.1f) * 50f);
+
+        float d = this.getMaxHealth() / a;
+        this.setHealth(this.getHealth() * d);
+    }
+
+    public void setSleepNeed() {
+        this.sleepNeed = sleep * 100f;
+    }
+
+    public float getSleepNeed() {
+        return sleepNeed;
+    }
+
+    public void setSleepNeed(float sleepNeed) {
+        this.sleepNeed = sleepNeed;
+    }
+
+    public Building getHome() {
+
+        if (this.home == null) {
+
+            for (GameObject object : s.objects) {
+                if (object instanceof Building) {
+                    Building b = (Building) object;
+                    if (!b.isFull()) {
+                        b.owned.add(this);
+                        this.home = b;
+                        return b;
+                    }
+                }
+            }
+            return null;
+        } else {
+            return home;
+        }
+    }
+
+    private Image headIcon;
+
+    @Override
+    public void setupSelectMenu(Table top, Table bottom) {
+        super.setupSelectMenu(top, bottom);
+        headIcon = new Image(this.job.getIcon());
+
+        bottom.add(headIcon).expand().size(Value.percentHeight(1f, bottom)).right();
+    }
+
+    @Override
+    public void updateSelectMenu() {
+        super.updateSelectMenu();
+        nameLabel.setText(this.getName() + " (" + age + ")");
+        headIcon.setDrawable(new SpriteDrawable(this.job.getIcon()));
+    }
+
+    public void setHome(Building home) {
+        this.home = home;
+    }
+
+
+    public void setFacing(Direction dir) {
+        this.dir = dir;
+    }
+
+    public Animation getTaskAnimation() {
+        return taskAnimation;
+    }
+
+    public void setTaskAnimation(Animation taskAnimation) {
+        this.taskAnimation = taskAnimation;
+    }
+
+    public Job getJob() {
+        return this.job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+        this.job.setHermit(this);
+    }
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
 }
