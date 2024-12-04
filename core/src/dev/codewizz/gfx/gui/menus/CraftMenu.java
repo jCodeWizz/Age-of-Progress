58c54ff929c39882dede8598321f1e91e2f11140
Cody <tom.heijkamp@outlook.com>
2024-12-04 19:12:19 +0100

    finished prototype crafting menu

--- core/src/dev/codewizz/gfx/gui/menus/CraftMenu.java
+++ core/src/dev/codewizz/gfx/gui/menus/CraftMenu.java
@@ -2,15 +2,27 @@ package dev.codewizz.gfx.gui.menus;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import dev.codewizz.gfx.gui.elements.UIImageButton;
 import dev.codewizz.gfx.gui.layers.GameLayer;
 import dev.codewizz.main.Main;
+import dev.codewizz.modding.Registers;
 import dev.codewizz.utils.Assets;
+import dev.codewizz.utils.Logger;
 import dev.codewizz.world.GameObject;
+import dev.codewizz.world.items.Recipe;
+import dev.codewizz.world.objects.tasks.CraftTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CraftMenu extends Menu {
+
+    public static List<Recipe> recipes = new ArrayList<>();
 
     private GameObject focus;
 
@@ -18,13 +30,30 @@ public class CraftMenu extends Menu {
 
     public CraftMenu(Stage stage, GameLayer layer) {
         super(stage, layer);
+
+
     }
 
     @Override
     protected void setup() {
+        recipes.add(Registers.recipes.get("aop:crude_axe"));
+
+
         main = new Table();
-        main.setBackground(new Image(Assets.getSprite("settlement-menu")).getDrawable());
-        base.add(main).expand().fill().size(100f, 100f);
+        Logger.log(recipes.size());
+
+        for (Recipe recipe : recipes) {
+            UIImageButton button = UIImageButton.create(UIImageButton.buySlotStyle, recipe.getResult()[0].item.getSprite());
+            button.addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    Main.inst.world.settlement.addTask(new CraftTask(recipe), true);
+                }
+            });
+            main.add(button).size(64, 64);
+        }
+
+        base.add(main).expand().size(64, 64);
     }
 
     public void open(GameObject object) {
